6cdd5e5702c991cd79f3cc0a641e5c6c02fc7ac2
renfufei <renfufei@qq.com>
2016-06-22 17:16:28 +0800

    template_url

--- clearpaper/footer-test.php
+++ clearpaper/footer-test.php
@@ -3,7 +3,12 @@
 	test内容:
 	
 	wp_title():  <?php wp_title(); ?>
+	
 	bloginfo("name"):  <?php bloginfo("name"); ?>
+	
+	get_template_directory_uri():  <?php get_template_directory_uri(); ?>
+	
+	bloginfo("template_url"):  <?php bloginfo("template_url"); ?>
 
 
 
--- clearpaper/header.php
+++ clearpaper/header.php
@@ -9,8 +9,9 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="generator" content="WordPress 4.5.2" />
 	<meta name="orig" content="来自于 twentysixteen 主题" />
+	<meta name="description" content="<?php bloginfo("description"); ?>" />
 	<title><?php bloginfo("name"); ?></title>
-	<link rel="stylesheet" href="/wp-content/themes/clearpaper-1/style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/style.css" type="text/css" media="screen" />
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
