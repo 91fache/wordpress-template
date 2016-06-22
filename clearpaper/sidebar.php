3d76248dbf4f0bdecfe7b3011c2c0d5b8abea71c
renfufei <renfufei@qq.com>
2016-06-22 18:48:41 +0800

    部分效果

--- clearpaper/sidebar.php
+++ clearpaper/sidebar.php
@@ -7,9 +7,9 @@
 	<div id="info">
 		<a class="info_item" href="https://www.91fache.com/wp-login.php">登录</a>
 		<a class="info_item" href="https://github.com/91fache">GitHub</a>
-		<a href="http://www.91fache.com/" class="info_right info_item">&copy; 2016</a>
+		<a href="http://www.91fache.com/" class="info_item">&copy; 2016</a>
 		<a href="https://www.91fache.com/wp-login.php?action=logout&_wpnonce=55a3671ac2"
-		 class="info_about info_item">登出</a>
+		 class="info_right">登出</a>
 		<div class="clear"></div>
 	</div><!-- #info end -->
 		
