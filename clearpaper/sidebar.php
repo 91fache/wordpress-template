561c77ec22a20e733e95947be55c762debbb372d
renfufei <renfufei@qq.com>
2016-06-22 18:58:18 +0800

    部分

--- clearpaper/sidebar.php
+++ clearpaper/sidebar.php
@@ -7,9 +7,8 @@
 	<div id="info">
 		<a class="info_item" href="https://www.91fache.com/wp-login.php">登录</a>
 		<a class="info_item" href="https://github.com/91fache">GitHub</a>
-		<a href="http://www.91fache.com/" class="info_item">&copy; 2016</a>
-		<a href="https://www.91fache.com/wp-login.php?action=logout&_wpnonce=55a3671ac2"
-		 class="info_right">登出</a>
+		<a href="http://www.91fache.com/" class="info_right">&copy; 2016</a>
+		
 		<div class="clear"></div>
 	</div><!-- #info end -->
 		
