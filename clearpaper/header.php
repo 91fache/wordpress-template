96080ac8e30141262e3d26ac0fa2d4d9cb7e17de
renfufei <renfufei@qq.com>
2016-06-22 16:20:28 +0800

    部分head

--- clearpaper/header.php
+++ clearpaper/header.php
@@ -1,15 +1,17 @@
 <?php
-// 引入 header.php
+// header.php
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>就要发车</title>
-<link rel="stylesheet" href="/wp-content/themes/clearpaper-1/style.css" type="text/css" media="screen" />
-<meta name="generator" content="WordPress 4.5.2" />
-<meta name="orig" content="来自于 twentysixteen 主题" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>就要发车</title>
+	<link rel="stylesheet" href="/wp-content/themes/clearpaper-1/style.css" type="text/css" media="screen" />
+	<meta name="generator" content="WordPress 4.5.2" />
+	<meta name="orig" content="来自于 twentysixteen 主题" />
 </head>
 <body>
 
