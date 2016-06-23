fe281d7f4de94608d4119cb28ae67c455f54781b
renfufei <renfufei@qq.com>
2016-06-23 19:39:41 +0800

    修改登录页链接

--- clearpaper/sidebar.php
+++ clearpaper/sidebar.php
@@ -5,8 +5,8 @@
 <div id="left">
 	
 	<div id="info">
-		<a class="info_item" href="https://www.91fache.com/wp-login.php">登录</a>
-		<a class="info_item" href="https://github.com/91fache">GitHub</a>
+		<a class="info_item" target="_blank" href="https://www.91fache.com/wp-admin/">登录</a>
+		<a class="info_item" target="_blank" href="https://github.com/91fache">GitHub</a>
 		<a href="http://www.91fache.com/" class="info_right">&copy; 2016</a>
 		
 		<div class="clear"></div>
