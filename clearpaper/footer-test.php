4bb4b4b96260887a54a6870d4522b875577d2268
renfufei <renfufei@qq.com>
2016-06-23 10:53:20 +0800

    丑

--- clearpaper/footer-test.php
+++ clearpaper/footer-test.php
@@ -13,6 +13,22 @@
 	bloginfo("template_url"):  <?php bloginfo("template_url"); ?>
 
 	next_posts_link(__("下一页")): <?php next_posts_link(__("下一页")) ?>
+	
+	wp_head(): 
+	
+	<?php 
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	
+	wp_head(); 
+	
+	?>
+
+
+
+other:
 
 
 安装 Disable Google Fonts 插件
--- clearpaper/header.php
+++ clearpaper/header.php
@@ -15,6 +15,15 @@
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
+		<?php 
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		
+	wp_head(); 
+	
+	?>
 </head>
 <body>
 
