0ee3a2c04820b9b2aa6a053f8cbd939b09991b3d
renfufei <renfufei@qq.com>
2016-07-08 11:25:31 +0800

    处理好底部栏目沾滞的问题

--- clearpaper/footer.php
+++ clearpaper/footer.php
@@ -4,12 +4,26 @@
 
 <?php 
 	// 引入侧边栏  sidebar.php
-	get_sidebar(); 
+	// get_sidebar(); 
 ?>
 
 <div class="clear"></div>
 
 </div><!-- wrap end -->
+
+<footer class="footer">
+  <div class="container col-center-block" id="info">
+		<a class="info_item" target="_blank" href="https://www.91fache.com/wp-admin/">登录</a>
+		<a class="info_item" target="_blank" href="https://github.com/91fache">GitHub</a>
+		<a href="http://www.91fache.com/" class="info_right">&copy; 2016</a>
+  </div>
+</footer>
+
+<!-- location.protocol 不是  https: 则应该变为http-->
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </body>
 </html>
