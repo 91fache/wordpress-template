3f11bcc57e76f0a61b978e6a4429157bf2a6080e
renfufei <renfufei@qq.com>
2022-01-11 22:11:52 +0800

    top

--- clearpaper/header-top.php
+++ clearpaper/header-top.php
@@ -5,14 +5,14 @@
 <div id="top" class="top">
 	<div id="logo"  class="inline">
 		<h1 class="inline">
-			<a href="http://www.91fache.com/">
+			<a href="/">
 				<span  class="logo_wrap">FACHE</span>
 			</a>
 		</h1>
 	</div><!-- #logo end -->
-	
+
 	<div id="head" class="inline">
 		 <a href="/" class="head_content">就要发车</a>
 	</div><!-- #head end -->
-	
+
 </div><!-- #top end -->
